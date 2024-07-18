278f86ff9e944f8c01c39b27b22a07b22e78e826
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-18 19:06:41 -0300

    Configura inicialmente controllers de Address e Person

---
+++ PeopleUXComex.Web/Controllers/AddressController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleUXComex.Application.Services;
+using PeopleUXComex.Core.Entities;
+using System.Threading.Tasks;
+
+namespace PeopleUXComex.Web.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly AddressService _addressService;
+
+        public AddressController(AddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        public async Task<ActionResult> Index(int personId)
+        {
+            var addresses = await _addressService.GetByPersonIdAsync(personId);
+            ViewBag.PersonId = personId;
+            return View(addresses);
+        }
+
+        public ActionResult Create(int personId)
+        {
+            ViewBag.PersonId = personId;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                await _addressService.AddAsync(address);
+                return RedirectToAction("Index", new { personId = address.PersonId });
+            }
+            return View(address);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var address = await _addressService.GetByIdAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                await _addressService.UpdateAsync(address);
+                return RedirectToAction("Index", new { personId = address.PersonId });
+            }
+            return View(address);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var address = await _addressService.GetByIdAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            return View(address);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id, int personId)
+        {
+            await _addressService.DeleteAsync(id);
+            return RedirectToAction("Index", new { personId });
+        }
+    }
+}
---
+++ PeopleUXComex.Web/Controllers/PersonController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleUXComex.Application.Services;
+using PeopleUXComex.Core.Entities;
+
+namespace PeopleUXComex.Web.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly PersonService _personService;
+
+        public PersonController(PersonService personService)
+        {
+            _personService = personService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var people = await _personService.GetAllAsync();
+            return View(people);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                await _personService.AddAsync(person);
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                await _personService.UpdateAsync(person);
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var person = await _personService.GetByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            await _personService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
