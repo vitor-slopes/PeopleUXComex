3f334ab0d69e236e738679ec504ff73312b9947e
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-17 23:18:21 -0300

    inicia criação da infrastructure

---
+++ PeopleUXComex.Infrastructure/Data/AddressRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using PeopleUXComex.Core.Entities;
+using PeopleUXComex.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleUXComex.Infrastructure.Data
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly string _connectionString;
+
+        public AddressRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Address>> GetByPersonIdAsync(int personId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT * FROM Addresses WHERE PersonId = @PersonId";
+                return await connection.QueryAsync<Address>(sql, new { PersonId = personId });
+            }
+        }
+
+        public async Task<Address?> GetByIdAsync(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT * FROM Addresses WHERE Id = @Id";
+                return await connection.QueryFirstOrDefaultAsync<Address>(sql, new { Id = id });
+            }
+        }
+
+        public async Task AddAsync(Address address)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "INSERT INTO Addresses (Street, CEP, City, State, PersonId) VALUES (@Street, @CEP, @City, @State, @PersonId)";
+                await connection.ExecuteAsync(sql, address);
+            }
+        }
+
+        public async Task UpdateAsync(Address address)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "UPDATE Addresses SET Street = @Street, CEP = @CEP, City = @City, State = @State WHERE Id = @Id";
+                await connection.ExecuteAsync(sql, address);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "DELETE FROM Addresses WHERE Id = @Id";
+                await connection.ExecuteAsync(sql, new { Id = id });
+            }
+        }
+    }
+}
---
+++ PeopleUXComex.Infrastructure/Data/PersonRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using PeopleUXComex.Core.Entities;
+using PeopleUXComex.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace PeopleUXComex.Infrastructure.Data
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly string _connectionString;
+
+        public PersonRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Person>> GetAllAsync()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return await connection.QueryAsync<Person>("SELECT * FROM Person");
+            }
+        }
+
+        public async Task<Person> GetByIdAsync(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return await connection.QueryFirstOrDefaultAsync<Person>("SELECT * FROM Person WHERE Id = @Id", new { Id = id });
+            }
+        }
+
+        public async Task AddAsync(Person person)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "INSERT INTO Person (Name, Phone, CPF) VALUES (@Name, @Phone, @CPF)";
+                await connection.ExecuteAsync(sql, person);
+            }
+        }
+
+        public async Task UpdateAsync(Person person)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "UPDATE Person SET Name = @Name, Phone = @Phone, CPF = @CPF WHERE Id = @Id";
+                await connection.ExecuteAsync(sql, person);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "DELETE FROM Person WHERE Id = @Id";
+                await connection.ExecuteAsync(sql, new { Id = id });
+            }
+        }
+    }
+}
