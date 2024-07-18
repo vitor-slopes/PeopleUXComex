798ff8df8ebb4a7e0a41634c86157961c7fbbac9
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-18 13:18:06 -0300

    atualiza context

---
+++ PeopleUXComex.Infrastructure/Data/PeopleUXComexContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleUXComex.Core.Entities;
+
+namespace PeopleUXComex.Infrastructure.Data
+{
+    public class PeopleUXComexContext : DbContext
+    {
+        public PeopleUXComexContext(DbContextOptions<PeopleUXComexContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
