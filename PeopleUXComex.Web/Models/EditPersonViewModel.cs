76362a76c0715539801701d061713030a87c67f6
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-19 02:40:04 -0300

    Cria model de edição

---
+++ PeopleUXComex.Web/Models/EditPersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PeopleUXComex.Core.Entities;
+using System.Collections.Generic;
+
+namespace PeopleUXComex.Web.Models
+{
+    public class EditPersonViewModel
+    {
+        public Person Person { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+        public Address NewAddress { get; set; } = new Address();
+    }
+}
