f1c1c7daa7c3e7aeb5bb197913b21ac2c0ba2df2
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-19 17:36:05 -0300

    Configura inicialmente o SQL Server Database Project

---
+++ PeopleUXComexDb/dbo/Tables/Address.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Address] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [PersonId] INT            NOT NULL,
+    [Street]   NVARCHAR (200) NOT NULL,
+    [CEP]      NVARCHAR (8)   NOT NULL,
+    [City]     NVARCHAR (100) NOT NULL,
+    [State]    NVARCHAR (2)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Persons] ([Id])
+);
+
